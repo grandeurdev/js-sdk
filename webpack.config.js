2149438adaa236bf5e9084a8ef99a02845156f13
Moiz Husnain <moiz@grandeur.tech>
2022-06-03 05:01:54 +0500

    Upgraded to webpack5 Now creating two separate builds for commonjs and umd

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,29 @@ const path = require("path");
 
 // Function to generate configurations
 const config = (bundle, target) => ({
-    target: target,
+    target: "web",
     entry: "./index.js",
-    mode: "production",
+    mode: "development",
     devServer: {
         static: './dist',
     },
     resolve: {
         fallback: {
-            "Buffer": require.resolve('buffer/'),
-            "stream": require.resolve("stream-browserify")
+            "Buffer": require.resolve('buffer'),
+            "stream": require.resolve("stream-browserify"),
+            "crypto": require.resolve("crypto-browserify")
         }
     },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: bundle,
         library: "grandeur",
+        libraryTarget: target,
         globalObject: "this"
     }
 })
 
 module.exports = [
-    config("grandeur-node.js", "node"),
-    config("grandeur.js", "web")
+    config("grandeur.js", "commonjs2"),
+    config("grandeur.umd.js", "umd")
 ]
